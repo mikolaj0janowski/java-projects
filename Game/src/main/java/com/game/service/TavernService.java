198637c44701306c733dda0d270033bf8a19a564
MikolajJanowski <mikolajjakubjanowski@gmail.com>
2020-12-30 17:42:16 +0100

    Added and implemented TavernService.

---
+++ Game/src/main/java/com/game/service/TavernService.java
@@ -0,0 +1,10 @@
+package com.game.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TavernService {
+    //This will be implemented later.
+}
